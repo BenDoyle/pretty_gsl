c3bf54d91791cc91f35ca1882317fdbf5d7d6585
Ben Doyle <ben.doyle@shopify.com>
2012-10-01 16:19:50 -0400

    implement indifference to presence of gradient function

--- minimize.rb
+++ minimize.rb
@@ -17,30 +17,65 @@ class Minimize
       direction_tolerance:          1e-4, # the tolerance for errors in the direction of line minimization
       absolute_gradient_tolerance:  1e-3, # halt if the magnetude of the gradient falls below this value
     }.merge(options)
-    @result = nil
+    @result    = nil
     self
   end
 
   def minimize(*guess)
-    loss = Function_fdf.alloc(@loss, @params[:loss_gradient], guess.size)
-    loss.set_params(@constants)
-    x = Vector.alloc(*guess)
-    minimizer = FdfMinimizer.alloc("conjugate_fr", guess.size)
-    minimizer.set(loss, x, @params[:step_size], @params[:direction_tolerance])
+    minimizer = get_gsl_minimizer(guess)
     @params[:max_iterations].times do |iter|
       status = minimizer.iterate
-      status = minimizer.test_gradient @params[:absolute_gradient_tolerance]
+      status = test_gsl_convergence(minimizer)
       if status != CONTINUE
         @result = {
           success:    status == SUCCESS,
           minimum_x:  minimizer.x.to_a,
-          minimum_f:  f = minimizer.f,
           iterations: iter
-        }
+        }.merge(get_gsl_result(minimizer))
         break
       end
     end
     @result
+  end
+
+  protected
+  def test_gsl_convergence(minimizer)
+    if @params[:loss_gradient]
+      minimizer.test_gradient @params[:absolute_gradient_tolerance]
+    else
+      minimizer.test_size @params[:absolute_gradient_tolerance]
+    end
+  end
+
+  def get_gsl_minimizer(guess)
+    minimizer = nil
+    if @params[:loss_gradient]
+      loss = Function_fdf.alloc(@loss, @params[:loss_gradient], guess.size)
+      loss.set_params(@constants)
+      minimizer = FdfMinimizer.alloc("conjugate_fr", guess.size)
+      minimizer.set(loss, Vector.alloc(*guess), @params[:step_size], @params[:direction_tolerance])
+    else
+      loss = Function.alloc(@loss, guess.size)
+      loss.set_params(@constants)
+      minimizer = FMinimizer.alloc("nmsimplex", guess.size)
+      ss = Vector.alloc(guess.size)
+      ss.set_all(@params[:step_size])
+      minimizer.set(loss, Vector.alloc(*guess), ss)
+    end
+    minimizer
+  end
+
+  def get_gsl_result(minimizer)
+    if @params[:loss_gradient]
+      {
+        minimum_f:  minimizer.f,
+      }
+    else
+      {
+        minimum_f:    minimizer.fval,
+        simplex_size: minimizer.size,
+      }
+    end
   end
 end
 
