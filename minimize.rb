fccef70b675437fb0fb8e740e64b4022e75f0bf9
Ben Doyle <ben.doyle@shopify.com>
2012-10-01 15:10:16 -0400

    clean up magic numbers

--- minimize.rb
+++ minimize.rb
@@ -1,35 +1,42 @@
 require 'gsl'
 #require 'debug'
 
-class Mimimize
+class Minimize
   include GSL
   include GSL::MultiMin
 
-  def initialize(loss, constants, gradient = nil)
-    @loss      = loss
-    @gradient  = gradient
-    @constants = constants
+  attr_accessor :verbose
+
+  def initialize(loss, constants, options = {})
+    @loss            = loss
+    @constants       = constants
+    @params = {
+      max_iterations: 100,
+      foo:            0.01,
+      bar:            1e-4,
+      baz:            1e-3,
+    }.merge(options)
   end
 
   def minimize(*guess)
-    my_func = Function_fdf.alloc(@loss, @gradient, guess.size)
+    my_func = Function_fdf.alloc(@loss, @params[:gradient], guess.size)
     my_func.set_params(@constants)      # parameters
 
     x = Vector.alloc(*guess)          # starting point
 
     minimizer = FdfMinimizer.alloc("conjugate_fr", guess.size)
-    minimizer.set(my_func, x, 0.01, 1e-4)
+    minimizer.set(my_func, x, @params[:foo], @params[:bar])
 
     iter = 0
     begin
       iter += 1
       status = minimizer.iterate()
-      status = minimizer.test_gradient(1e-3)
+      status = minimizer.test_gradient(@params[:baz])
       puts("Minimum found at") if status == GSL::SUCCESS
       x = minimizer.x
       f = minimizer.f
       printf("%5d %.5f %.5f %10.5f\n", iter, x[0], x[1], f)
-    end while status == GSL::CONTINUE and iter < 100
+    end while status == GSL::CONTINUE and iter < @params[:max_iterations]
   end
 end
 
